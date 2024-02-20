a4568ceda196c898866213741a3b0eae44e9bb41
JohnsonOdang <johnnyodanny@gmail.com>
2024-02-20 01:50:34 -0800

    creating and collection

--- Node_Mongo/create_collection.js
+++ Node_Mongo/create_collection.js
@@ -1,1 +1,16 @@
-// Creating a collection with
+// Creating a collection and inserting a document
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  // insert into collection
+  var myobj = { name: "Company Inc", address: "Highway 37" };
+
+  dbo.createCollection("customers").insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    console.log("one document inserted");
+    db.close();
+  });
+});
